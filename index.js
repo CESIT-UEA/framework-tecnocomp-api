fb2738cbddf2acb6b2574127f58f7045262569d4
Henrique <henrixe80@gmail.com>
2024-10-23 15:48:06 -0400

    Tentando 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+const fs = require('fs');
+const https = require('https');
 const express = require("express");
 const { Sequelize, Op } = require("sequelize");
 const LtiSequelize = require("ltijs-sequelize");
