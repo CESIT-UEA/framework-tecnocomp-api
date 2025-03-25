0e6b111ed6151c817948baeb05fcd95fb9837013
Henrique <henrixe80@gmail.com>
2025-03-25 16:18:13 -0400

    Corrigindo

--- models/index.js
+++ models/index.js
@@ -3,8 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  port: 3306,
-  re
+  port: 3306
 });
 
 const db = {};
